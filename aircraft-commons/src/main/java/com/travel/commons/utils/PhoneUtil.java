caecb6b8d1df1f413a45bb602d9a04ff974e3ba8
qiurunze <qiurunze@youxin.com>
2019-11-05 21:18:28 +0800

    start init

---
+++ aircraft-commons/src/main/java/com/travel/commons/utils/PhoneUtil.java
@@ -0,0 +1,25 @@
+package com.travel.commons.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneUtil {
+
+	private static String REGEX = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+	private static Pattern P = Pattern.compile(REGEX);
+
+	/**
+	 * 校验手机号
+	 * 
+	 * @param phone
+	 * @return
+	 */
+	public static boolean checkPhone(String phone) {
+		if (phone == null || phone.length() != 11) {
+			return Boolean.FALSE;
+		}
+
+		Matcher m = P.matcher(phone);
+		return m.matches();
+	}
+}
---
+++ aircraft-service/src/main/java/com/travel/service/ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travel.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
