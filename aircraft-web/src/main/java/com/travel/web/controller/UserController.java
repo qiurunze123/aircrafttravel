2fedc42566b37f9f80d29031163000706dffbf71
qiurunze <qiurunze@youxin.com>
2019-11-06 21:05:23 +0800

    craft

--- aircraft-web/src/main/java/com/travel/web/controller/UserController.java
+++ aircraft-web/src/main/java/com/travel/web/controller/UserController.java
@@ -1,7 +1,33 @@
 package com.travel.web.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.travel.function.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author 邱润泽 bullock
  */
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value="/list", produces="text/html")
+    @ResponseBody
+    public String list() {
+
+        List<String> list = Arrays.asList("1","2","3");
+        List<String> list1 = list.stream().filter((String str)->
+                str.equals("2")).collect(Collectors.toList());
+        return JSON.toJSONString(list1);
+    }
 }
