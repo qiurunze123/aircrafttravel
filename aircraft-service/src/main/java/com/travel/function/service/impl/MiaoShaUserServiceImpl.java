a356db8bc28898229a70c73d6aa587bdd69eb0e9
QiuRunZe <QiuRunZe_key@163.com>
2019-11-10 18:41:19 +0800

    分布式session

--- aircraft-service/src/main/java/com/travel/function/service/impl/MiaoShaUserServiceImpl.java
+++ aircraft-service/src/main/java/com/travel/function/service/impl/MiaoShaUserServiceImpl.java
@@ -93,7 +93,6 @@ public class MiaoShaUserServiceImpl implements MiaoShaUserService {
             return null;
         }
         MiaoShaUser user = (MiaoShaUser) redisClient.get(MiaoShaUserKey.token,token,MiaoShaUser.class);
-
         if(user!=null){
             addCookie(response,token,user);
         }
